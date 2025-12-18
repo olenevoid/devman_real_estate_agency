0a53c8b069b20cbca60f193200503349c936f8f9
olenevoid <mazekunov@gmail.com>
2025-12-18 11:06:16 +0500

    Make created_at read only

--- property/admin.py
+++ property/admin.py
@@ -5,3 +5,4 @@ from .models import Flat
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'town_district', 'address', 'owner')
+    readonly_fields = ('created_at',)
