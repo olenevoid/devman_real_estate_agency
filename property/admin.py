bb6466c93af0b72612e694e2304a63c2b4924a74
olenevoid <mazekunov@gmail.com>
2026-01-14 10:50:26 +0500

    add admin UI for complaints

--- property/admin.py
+++ property/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Flat
+from .models import Flat, Complain
 
 
 @admin.register(Flat)
@@ -9,3 +9,8 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+
+
+@admin.register(Complain)
+class ComplainAdmin(admin.ModelAdmin):
+    raw_id_fields = ['flat']
